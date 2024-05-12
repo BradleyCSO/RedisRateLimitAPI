7beb8a1e237169a205e7fa3019e0fca900698de1
BradleyCSO <Osborne.Bradley1998@gmail.com>
2024-05-12 13:08:12 +0100

    Remove unnecessary close async (connection disposes itself)

--- Services/RedisRateLimitService.cs
+++ Services/RedisRateLimitService.cs
@@ -13,33 +13,26 @@ public class RedisRateLimitService(ConnectionMultiplexer connection) : IRedisRat
         if (rateLimit == null)
             return null;
 
-        try
+        // Use cache key for this endpoint
+        string cacheKey = $"RateLimit:{endpoint}";
+
+        // Count the number of requests in this window for this given endpoint
+        RedisValue requestsThisWindowCount = await connection.GetDatabase().StringGetAsync(cacheKey);
+
+        if (!requestsThisWindowCount.IsNull)
         {
-            // Use cache key for this endpoint
-            string cacheKey = $"RateLimit:{endpoint}";
+            // Increment the existing count
+            int newCount = (int)requestsThisWindowCount + 1;
 
-            // Count the number of requests in this window for this given endpoint
-            RedisValue requestsThisWindowCount = await connection.GetDatabase().StringGetAsync(cacheKey);
+            // Check if the rate limit has been exceeded
+            if (newCount > rateLimit.RequestLimit)
+                return true;
 
-            if (!requestsThisWindowCount.IsNull)
-            {
-                // Increment the existing count
-                int newCount = (int)requestsThisWindowCount + 1;
-
-                // Check if the rate limit has been exceeded
-                if (newCount > rateLimit.RequestLimit)
-                    return true;
-
-                // Update the count in Redis
-                await connection.GetDatabase().StringSetAsync(cacheKey, newCount, TimeSpan.FromSeconds(rateLimit.Window));
-            }
-            else // Set the initial count for this endpoint
-                await connection.GetDatabase().StringSetAsync(cacheKey, 1, TimeSpan.FromSeconds(rateLimit.Window));
+            // Update the count in Redis
+            await connection.GetDatabase().StringSetAsync(cacheKey, newCount, TimeSpan.FromSeconds(rateLimit.Window));
         }
-        finally
-        {
-            await connection.CloseAsync();
-        }
+        else // Set the initial count for this endpoint
+            await connection.GetDatabase().StringSetAsync(cacheKey, 1, TimeSpan.FromSeconds(rateLimit.Window));
 
         return false;
     }
