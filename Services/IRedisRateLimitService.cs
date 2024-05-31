d5b3872a7034855843d914121f6082217b971390
BradleyCSO <Osborne.Bradley1998@gmail.com>
2024-05-31 16:04:24 +0100

    Update IRedisRateLimitService with docstrings detailing functionality

--- Services/IRedisRateLimitService.cs
+++ Services/IRedisRateLimitService.cs
@@ -1,6 +1,19 @@
 ﻿namespace RedisRateLimitAPI.Services;
 
+/// <summary>
+///     Service responsible for rate limiting a request 
+///     provided that it has been configured in the rate_limits.json <see cref="Models.RateLimitConfig"/>
+/// </summary>
 public interface IRedisRateLimitService
 {
-    Task<bool?> IsRequestRateLimitedAsync(string endpoint);
+	/// <summary>
+	/// Checks if a rate limit key exists for a given endpoint
+	/// </summary>
+	/// <param name="endpoint">The key we're querying to determine whether this endpoint has a rate limit configured</param>
+	/// <returns>
+		/// If a key exists: add one, return false,
+		/// If key exists and is below configured limit: increment the count, return false
+		/// If key exists, and exceeds the configured limit: return true
+	/// </returns>
+	Task<bool?> IsRequestRateLimitedAsync(string endpoint);
 }
