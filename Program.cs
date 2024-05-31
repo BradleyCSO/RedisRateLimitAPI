fa4221cbeb530911a2442bd6e7d58a420006747c
BradleyCSO <Osborne.Bradley1998@gmail.com>
2024-05-31 15:45:46 +0100

    Update Program to use top-level statements

--- Program.cs
+++ Program.cs
@@ -2,37 +2,29 @@ using RedisRateLimitAPI.Models;
 using RedisRateLimitAPI.Services;
 using StackExchange.Redis;
 
-namespace RedisRateLimitAPI;
-
-public class Program
+WebApplicationBuilder? builder = WebApplication.CreateSlimBuilder(args);
+builder.Services.ConfigureHttpJsonOptions(options =>
 {
-    static void Main(string[] args)
-    {
-        WebApplicationBuilder? builder = WebApplication.CreateSlimBuilder(args);
-        builder.Services.ConfigureHttpJsonOptions(options =>
-        {
-            options.SerializerOptions.TypeInfoResolverChain.Insert(0, SourceGenerationContext.Default);
-        });
-        builder.Services.AddSingleton<IRedisRateLimitService, RedisRateLimitService>(provider =>
-        {
-            ConnectionMultiplexer connection = ConnectionMultiplexer.Connect("localhost,abortConnect=false");
-            return new RedisRateLimitService(connection);
-        });
+	options.SerializerOptions.TypeInfoResolverChain.Insert(0, SourceGenerationContext.Default);
+});
+builder.Services.AddSingleton<IRedisRateLimitService, RedisRateLimitService>(provider =>
+{
+	ConnectionMultiplexer connection = ConnectionMultiplexer.Connect("localhost,abortConnect=false");
+	return new RedisRateLimitService(connection);
+});
 
-        WebApplication? app = builder.Build();
+WebApplication? app = builder.Build();
 
-        app.MapGet("{**endpoint}", async (HttpContext context, IRedisRateLimitService redisRateLimitService) =>
-        {
-            bool? isRateLimited = await redisRateLimitService.IsRequestRateLimitedAsync(context.Request.Path);
+app.MapGet("{**endpoint}", async (HttpContext context, IRedisRateLimitService redisRateLimitService) =>
+{
+	bool? isRateLimited = await redisRateLimitService.IsRequestRateLimitedAsync(context.Request.Path);
 
-            if (isRateLimited == null)
-                return Results.StatusCode(403); // We haven't defined a rate limit for this endpoint in rate_limits.json
-            else if (isRateLimited ?? false)
-                return Results.StatusCode(429);
-            else
-                return Results.Accepted();
-        });
+	if (isRateLimited == null)
+		return Results.StatusCode(403); // We haven't defined a rate limit for this endpoint in rate_limits.json
+	else if (isRateLimited ?? false)
+		return Results.StatusCode(429);
+	else
+		return Results.Accepted();
+});
 
-        app.Run();
-    }
-}
+app.Run();
